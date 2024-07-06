66f10a95a625860a7d4b1191dd15994b65b15371
rMaxiQp <zq2@illinois.edu>
2024-07-06 23:03:37 +0900

    Complete JS examples

--- src/client.ts
+++ src/client.ts
@@ -3,13 +3,13 @@ import { createGrpcTransport } from "@connectrpc/connect-node";
 import { Greet } from "../proto/greet/greet_connect.js";
 
 const transport = createGrpcTransport({
-  baseUrl: "http://localhost:8080",
+  baseUrl: "http://localhost:9000",
   httpVersion: "2",
 });
 
 async function main() {
   const client = createPromiseClient(Greet, transport);
-  const response = await client.sayHello({ name: "world" });
+  const response = await client.sayHello({ name: "world", language: "es" });
   console.log("response: ", response.message);
 }
 
--- src/server.ts
+++ src/server.ts
@@ -1,39 +1,46 @@
-import https from "node:https";
+import http2 from "node:http2";
+import { connectNodeAdapter } from "@connectrpc/connect-node";
+import { ConnectRouter, Interceptor } from "@connectrpc/connect";
+import { Greet } from "../proto/greet/greet_connect.js";
 import { HelloRequest, HelloResponse } from "../proto/greet/greet_pb.js";
 
-function greet(payload: HelloRequest): Promise<HelloResponse> {
-  return new Promise((resolve, reject) => {
-    const req = https.request(
-      {
-        host: "localhost",
-        port: 8080,
-        method: "POST",
-      },
-      (res) => {
-        let data = "";
-        res.on("data", (chunk) => {
-          data += chunk;
-        });
-        res.on("end", () => {
-          resolve(HelloResponse.fromJson(data));
-        });
-        res.on("error", reject);
-      }
-    );
+const sayHello = async function (req: HelloRequest): Promise<HelloResponse> {
+  const res = new HelloResponse();
+  let prefix = "Hello";
+  switch (req.language.toLowerCase()) {
+    case "es":
+      prefix = "Hola";
+      break;
+    case "fr":
+      prefix = "Bonjour";
+      break;
+    case "de":
+      prefix = "Hallo";
+      break;
+    default:
+      break;
+  }
+  res.message = `${prefix}, ${req.name}`;
+  return res;
+};
 
-    req.write(JSON.stringify(payload));
-    req.end();
-  });
+function logger(): Interceptor {
+  return (next) => async (req) => {
+    const start = Date.now();
+    const res = await next(req);
+    console.log(
+      `Request ${req.method.name} took ${(Date.now() - start).toString()}ms`
+    );
+    return res;
+  };
 }
 
-async function main() {
-  // const server = new Server();
-  // server.addService(Greet, { sayHello });
-  // client.makeUnaryRequest()
+const routes = function (router: ConnectRouter) {
+  router.service(Greet, { sayHello }, { interceptors: [logger()] });
+};
 
-  const payload = new HelloRequest({ name: "world" });
-  await greet(payload);
-  console.log("Hello, world!");
+async function main() {
+  http2.createServer(connectNodeAdapter({ routes })).listen(9000);
 }
 
 main();
