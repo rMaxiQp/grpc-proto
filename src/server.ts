f41c9418e19ede809e7cbed7e23aaa6e1efc8c00
rMaxiQp <zq2@illinois.edu>
2024-07-09 17:26:13 +0900

    JS Server: Support gRPC & HTTP

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,4 @@
+import http, { type IncomingMessage, type ServerResponse } from "node:http";
 import http2 from "node:http2";
 import { connectNodeAdapter } from "@connectrpc/connect-node";
 import { ConnectRouter, Interceptor } from "@connectrpc/connect";
@@ -29,7 +30,7 @@ function logger(): Interceptor {
     const start = Date.now();
     const res = await next(req);
     const cost = (Date.now() - start).toString();
-    console.log(`Request ${req.method.name} took ${cost}ms`);
+    console.log(`RPC Request ${req.method.name} took ${cost}ms`);
     return res;
   };
 }
@@ -38,4 +39,26 @@ const routes = function (router: ConnectRouter) {
   router.service(Greet, { sayHello }, { interceptors: [logger()] });
 };
 
-http2.createServer(connectNodeAdapter({ routes })).listen(9000);
+const requestHandler = function (req: IncomingMessage, res: ServerResponse) {
+  const start = Date.now();
+  const query = new URL(req.url ?? "", "http://localhost:9000");
+  const payload = new HelloRequest({
+    language: query.searchParams.get("language") ?? "",
+    name: query.searchParams.get("name") ?? "",
+  });
+  res.setHeader("Content-Type", "application/json");
+  sayHello(payload).then((result) => {
+    res.end(JSON.stringify(result));
+    const cost = (Date.now() - start).toString();
+    console.log(`HTTP Request ${req.url} took ${cost}ms`);
+  });
+};
+
+http2.createServer(connectNodeAdapter({ routes })).listen(9090, () => {
+  console.log("RPC Server on 9090");
+});
+const httpServer = http.createServer();
+httpServer.on("request", requestHandler);
+httpServer.listen(9000, () => {
+  console.log("HTTP Server on 9000");
+});
